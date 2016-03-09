5b4907c51b7d666cf5680af0e371788a8092fecb
wlam02 <aviram.moran@gmail.com>
2016-03-09 15:13:23 -0600

    completed tested project

--- src/CurrencyConversion.java
+++ src/CurrencyConversion.java
@@ -1,33 +1,55 @@
+/***********************************************************************
+Program Name: CurrencyConversion.java
+Programmer's Name: Aviram Moran
+Program Description: This program converts currency between USD, YEN and European EURO
+***********************************************************************/
+
 import javax.swing.JOptionPane;
+import java.text.NumberFormat; //used to format to Currency
+import java.util.Locale;
 
 public class CurrencyConversion {
+	public static void main(String[] args) {
 
-public static void main(String[] args) {
+		double  Amount2COnvert = 0;
+		double result, result1, result2, result3, result4, result5;
+		String YEN = "YEN", EURO = "EURO", USD = "USD", currency = "";
+		NumberFormat toUSD = NumberFormat.getCurrencyInstance(Locale.US);; //use NumberFormat class to format double into Currency 
+		NumberFormat toEURO = NumberFormat.getCurrencyInstance(Locale.UK);
+		NumberFormat toYEN = NumberFormat.getCurrencyInstance(Locale.JAPANESE);
+		
+		
+		{
+			//Reform input validation to match one of three possible inputs
+			while ((!(currency.equals(USD) || currency.equals(YEN) || currency
+					.equals(EURO)))) {
+				currency = JOptionPane
+						.showInputDialog("Please select USD, YEN or EURO Currency to exchange: "
+								+ YEN + ", " + EURO + ", " + USD).toUpperCase();				
+			}
+		}
+		{
+			Amount2COnvert = Double.parseDouble(JOptionPane
+					.showInputDialog("Insert Amount to convert: "));
+			result = Amount2COnvert * 123.14; // Convert USD TO YEN
+			result1 = Amount2COnvert * .9; // Convert USD TO EURO
+			result2 = Amount2COnvert * .0081; // Convert YEN to USD
+			result3 = Amount2COnvert * .0073; // Convert YEN to EURO
+			result4 = Amount2COnvert * 1.11; // Convert EURO to USD
+			result5 = Amount2COnvert * 136.08; // Convert EURO to YEN
 
-int x = 0;
-double result,result1, result2;
-String THAI = "THAI", EURO ="EURO", JPY = "JPY";
-
-String name = JOptionPane.showInputDialog("What is your name: ");
-String message = String.format("Welcome %s, to the Currency Exchange Program ", name); /*inputs name in %s*/ 
-JOptionPane.showMessageDialog(null,  message); //displays the String format message
-String currency = JOptionPane.showInputDialog("Which currency do you wish to exchange: "+THAI+ ", "+EURO+ ", "+JPY);
-x = Integer.parseInt (JOptionPane.showInputDialog("Insert US Dollar Amount: "));
-result = x * 32.57; //US TO THAI
-result1 = x * .86; //US TO EURO
-result2 = x * 117.50; //US TO JPY
-
-
-if (currency == "THAI"){
-    JOptionPane.showMessageDialog(null,"US Dollar Amount of "+x+ " dollars to be converted to "+THAI+" is: ");
-    JOptionPane.showMessageDialog(null,result+ " BAHT");
-}if (currency == "EURO"){
-        JOptionPane.showMessageDialog(null,"The Amount of "+x+ " dollars to be converted to "+EURO+" is: ");
-        JOptionPane.showMessageDialog(null,result1+ " EURO");
-}if (currency == "JPY"){
-            JOptionPane.showMessageDialog(null,"The Amount of "+x+ " dollars to be converted to "+JPY+" is: ");
-            JOptionPane.showMessageDialog(null,result2+ " JPY");
-}//end THAI if
-
-
-}//end main
+			{
+				if (currency.equals("YEN")) {
+					JOptionPane.showMessageDialog(null, toYEN.format(Amount2COnvert) + " YEN" + " = "
+							+ toUSD.format(result2) + " USD" + " or " + toEURO.format(result3) + " EUR");
+				} else if (currency.equals("EURO")) {
+					JOptionPane.showMessageDialog(null, toEURO.format(Amount2COnvert) + " EURO" + " = "
+							+ toUSD.format(result4) + " USD" + " or " + toYEN.format(result5) + " YEN");
+				} else {
+					JOptionPane.showMessageDialog(null, toUSD.format(Amount2COnvert) + " USD" + " = "
+							+ toYEN.format(result) + " YEN" + " or " + toEURO.format(result1) + " EUR");
+				}
+			}
+		}
+	}
+}
